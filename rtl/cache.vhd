30e0d6c7fb07fb64b05b631def05817fb45803d9
Mark <yoflippo@gmail.com>
2017-05-15 16:14:02 +0200

    MS: update of file after synth error

--- rtl/cache.vhd
+++ rtl/cache.vhd
@@ -136,7 +136,7 @@ begin
         end if;
 
         if byte_we_next = "0000" or byte_we_next = "1111" then  --read or 32-bit write
-            cache_tag_in <= address_next(20 downto 13); -- MS: TAG still 2 MB
+            cache_tag_in <= '0' & address_next(20 downto 13); -- MS: TAG still 2 MB
         else
             cache_tag_in <= ONES(8 downto 0);  --invalid tag
         end if;
