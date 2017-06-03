878d96c10a2a01ef8f95a10b421893979b56b891
Yof <yoflippo@gmail.com>
2017-06-03 12:46:09 +0200

    ready to simulate cs-adder

--- adder_sim/adder.vhd
+++ adder_sim/adder.vhd
@@ -16,12 +16,10 @@ entity adder is
 end; --entity adder
 
 architecture logic of adder is
- 
+
     constant ci        : std_logic := '0';
 
-    signal adder0      : std_logic_vector(8 downto 0);
-    signal adder1_0    : std_logic_vector(8 downto 0);
-    signal adder1_1    : std_logic_vector(8 downto 0);
+    signal adder1      : std_logic_vector(8 downto 0);
     signal adder2_0    : std_logic_vector(8 downto 0);
     signal adder2_1    : std_logic_vector(8 downto 0);
     signal adder3_0    : std_logic_vector(8 downto 0);
@@ -37,47 +35,58 @@ architecture logic of adder is
     signal partresult2 : std_logic_vector(8 downto 0);
 
 begin
-    adder0              <= bv_adder(a(7 downto 0), b(7 downto 0), not ci);
-    adder1_0            <= bv_real_adder(a(15 downto 8), a(15 downto 8), not ci);
-    adder1_1            <= bv_real_adder(a(15 downto 8), a(15 downto 8), ci);
-    adder2_0            <= bv_real_adder(a(23 downto 16), a(23 downto 16), not ci);
-    adder2_1            <= bv_real_adder(a(23 downto 16), a(23 downto 16), ci);
-    adder3_0            <= bv_real_adder(a(31 downto 24), a(31 downto 24), not ci);
-    adder3_1            <= bv_real_adder(a(31 downto 24), a(31 downto 24), ci);
-    carry_0             <= adder0(8);
+    -- MS: carry-select adder based on Parhami
+    -- calculate different parts of words
+    adder1              <= bv_real_adder(a(7  downto 0 ), b(7  downto 0 ), not ci);
+    adder2_0            <= bv_real_adder(a(15 downto 8 ), a(15 downto 8 ), not ci);
+    adder2_1            <= bv_real_adder(a(15 downto 8 ), a(15 downto 8 ),     ci);
+    adder3_0            <= bv_real_adder(a(23 downto 16), a(23 downto 16), not ci);
+    adder3_1            <= bv_real_adder(a(23 downto 16), a(23 downto 16),     ci);
+    adder4_0            <= bv_real_adder(a(31 downto 24), a(31 downto 24), not ci);
+    adder4_1            <= bv_real_adder(a(31 downto 24), a(31 downto 24),     ci);
+    carry_0             <= adder1(8);
 
-    result(15 downto 0) <= adder1_1(7 downto 0) & adder0(7 downto 0)
-        when carry_0 = '1';
-        carry_1 <= adder1_1(8) when carry_0 = '1';
-        --pCarrySelect :process (a, b, do_add)
-        --begin
-        -- -- first half of two - level carry-select adder k/4-adder
-        -- if carry_0 = '1' then
-        -- result(15 downto 0) <= adder1_1 & adder0;
-        -- carry_1 <= adder1_1(8);
-        -- else
-        -- result(15 downto 0) <= adder1_0 & adder0;
-        -- carry_1 <= adder1_0(8);
-        -- end if;
+    -- MS: switch the right parts to the end result
+    result(15 downto 0)     <= adder2_1(7 downto 0) & adder1(7 downto 0)
+    when carry_0 = '1'else     adder2_0(7 downto 0) & adder1(7 downto 0);
 
-        -- -- first smaller last part
-        -- if adder2_1(8) = '1' then
-        -- partresult1 <= adder3_1;
-        -- else
-        -- partresult1 <= adder3_0;
-        -- end if;
+    carry_1                 <= adder2_1(8) 
+    when carry_0 = '1' else    adder2_0(8);
+ 
+    partresult1  <= adder4_1 when adder3_1(8) = '1' else adder4_0;
+    partresult2  <= adder4_1 when adder3_0(8) = '1' else adder4_0;
 
-        -- if adder2_0(8) = '1' then
-        -- partresult2 <= adder3_1;
-        -- else
-        -- partresult2 <= adder3_0;
-        -- end if;
+    result(32 downto 16)    <= partresult1(8 downto 0) & adder3_1(7 downto 0)
+    when carry_1 = '1' else    partresult2(8 downto 0) & adder3_0(7 downto 0);
+            --pCarrySelect :process (a, b, do_add)
+            --begin
+            -- -- first half of two - level carry-select adder k/4-adder
+            -- if carry_0 = '1' then
+            -- result(15 downto 0) <= adder2_1 & adder1;
+            -- carry_1 <= adder2_1(8);
+            -- else
+            -- result(15 downto 0) <= adder2_0 & adder1;
+            -- carry_1 <= adder2_0(8);
+            -- end if;
 
-        -- if carry_1 = '1' then
-        -- result(32 downto 16) <= partresult1(8 downto 0) & adder2_1(7 downto 0);
-        -- else
-        -- result(32 downto 16) <= partresult2(8 downto 0) & adder2_0(7 downto 0);
-        -- end if;
+            -- -- first smaller last part
+            -- if adder3_1(8) = '1' then
+            -- partresult1 <= adder3_1;
+            -- else
+            -- partresult1 <= adder3_0;
+            -- end if;
 
-        --end process;
+            -- if adder3_0(8) = '1' then
+            -- partresult2 <= adder3_1;
+            -- else
+            -- partresult2 <= adder3_0;
+            -- end if;
+
+            -- if carry_1 = '1' then
+            -- result(32 downto 16) <= partresult1(8 downto 0) & adder3_1(7 downto 0);
+            -- else
+            -- result(32 downto 16) <= partresult2(8 downto 0) & adder3_0(7 downto 0);
+            -- end if;
+
+            --end process;
 end; --architecture logic
--- adder_sim/tb_adder.vhd
+++ adder_sim/tb_adder.vhd
@@ -1,13 +1,13 @@
 --====================================================================================================================--
---  Title:        Testbench for adder_tb module
---  File Name:    adder_tb.vhd
---  Author:       MS
---  Date:         Thursday, June 01, 2017
+-- Title: Testbench for adder_tb module
+-- File Name: adder_tb.vhd
+-- Author: MS
+-- Date: Thursday, June 01, 2017
 --
---  Description:
---                * Provides stimuli to mul
---                * Verifies the output in response to the stimuli
---                * Reports the outcome of the verification on the console
+-- Description:
+-- * Provides stimuli to mul
+-- * Verifies the output in response to the stimuli
+-- * Reports the outcome of the verification on the console
 --
 --====================================================================================================================--
 library ieee;
@@ -16,126 +16,115 @@ use ieee.numeric_std.all;
 
 library work;
 use work.TxtUtil_pkg.all;
-use work.mlite_pack.all;  -- MS: for use of subtypes
-
-
-entity  adder_tb is
-end entity  adder_tb;
+use work.mlite_pack.all; -- MS: for use of subtypes
+entity adder_tb is
+end entity adder_tb;
 
 architecture sim of adder_tb is
 
    ---------------------------------------------------------------------------------------------------------------------
    -- Simulation parameters and signals
    ---------------------------------------------------------------------------------------------------------------------
-   constant cCLOCK_FREQ    : integer   := 100000000; -- MS: 100 MHz
-   constant cCLOCK_PERIOD  : time      := 1 sec / cCLOCK_FREQ;
+   constant cCLOCK_FREQ   : integer := 100000000; -- MS: 100 MHz
+   constant cCLOCK_PERIOD : time := 1 sec / cCLOCK_FREQ;
 
-   signal SimFinished      : boolean   := false;
+   signal SimFinished     : boolean := false;
 
    ---------------------------------------------------------------------------------------------------------------------
    -- Clocks and Resets
    ---------------------------------------------------------------------------------------------------------------------
 
-   signal Clk              : std_logic := '0';
-   signal Rst              : std_logic := '0';
-
-
+   signal Clk : std_logic := '0';
+   signal Rst : std_logic := '0';
    ---------------------------------------------------------------------------------------------------------------------
    -- Unit under test connections
    ---------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
+   signal a, b            : std_logic_vector(31 downto 0);
+   signal c               : std_logic_vector(32 downto 0);
+   signal do_add          : std_logic;
+   signal result_bv_adder : std_logic_vector(32 downto 0);
+   
 begin
-
    ---------------------------------------------------------------------------------------------------------------------
    -- Clock generation
    ---------------------------------------------------------------------------------------------------------------------
-   prClockGen: process is
+   prClockGen : process is
    begin
       Clk <= '0';
-      while NOT SimFinished loop
-         Clk <= NOT Clk;
-         wait for (cCLOCK_PERIOD / 2);
-      end loop;
-      echo("Simulation ended at: " & time'IMAGE(now));
-      wait;
+      while not SimFinished loop
+      Clk <= not Clk;
+      wait for (cCLOCK_PERIOD / 2);
+   end loop;
+   echo("Simulation ended at: " & time'IMAGE(now));
+   wait;
    end process prClockGen;
 
    ---------------------------------------------------------------------------------------------------------------------
    -- Unit under test
    ---------------------------------------------------------------------------------------------------------------------
-   --UUT: entity work.mult
-    -- port map(
-    --    clk       => Clk,
-    --    reset_in  => Rst,
-    --    a         => a,
-    --    b         => b,
-    --    mult_func => mult_func,
-    --    c_mult    => c_mult,
-    --    pause_out => pause_out
-    --);
-
-
+   UUT : entity work.adder
+      port map(
+         a      => a, 
+         b      => b, 
+         do_add => do_add, 
+         c      => c
+      );
    ---------------------------------------------------------------------------------------------------------------------
    -- Target model(s)
    ---------------------------------------------------------------------------------------------------------------------
-
-
    ---------------------------------------------------------------------------------------------------------------------
    -- Stimulation
    ---------------------------------------------------------------------------------------------------------------------
-   prStimuli: process
+   prStimuli           : process
       variable vSimResult : boolean := true;
    begin
-      Rst   <= '0';
+      Rst <= '0';
 
       wait until rising_edge(Clk);
-      Rst   <= '1';
+      Rst <= '1';
       wait until rising_edge(Clk);
       wait for cCLOCK_PERIOD * 10;
       wait until rising_edge(Clk);
-      Rst   <= '0';
+      Rst <= '0';
 
-    --  wait until rising_edge(Clk);
+      wait until rising_edge(Clk);
 
-    --  a         <= X"00000303";
-    --  b         <= X"00000002";
-    --  mult_func <= MULT_MULT; 		-- MS: choose type of multiplication
- 	  --wait until rising_edge(Clk);
-    --  mult_func <=  MULT_NOTHING;	-- MS: start multiplier
-    --  wait for cCLOCK_PERIOD * 11;
-    --  mult_func <= MULT_READ_LO;
+      a               <= X"00000303";
+      b               <= X"00000002";
+      do_add          <= '1';
 
+      result_bv_adder <= bv_adder(a, b, do_add);
 
- 	  --wait until rising_edge(pause_out);
+      -- mult_func <= MULT_MULT; -- MS: choose type of multiplication
+      --wait until rising_edge(Clk);
+      -- mult_func <= MULT_NOTHING; -- MS: start multiplier
+      -- wait for cCLOCK_PERIOD * 11;
+      -- mult_func <= MULT_READ_LO;
+      --wait until rising_edge(pause_out);
 
-    --  wait for cCLOCK_PERIOD * 10;
+      -- wait for cCLOCK_PERIOD * 10;
 
-    --  a         <= X"00000333";
-    --  b         <= X"00000002";
-    --  mult_func <= MULT_MULT; 
+      -- a <= X"00000333";
+      -- b <= X"00000002";
+      -- mult_func <= MULT_MULT;
 
-    --  wait for cCLOCK_PERIOD * 10;
-      
-
+      -- wait for cCLOCK_PERIOD * 10;
       if (vSimResult) then
          echo("-----------------------------------------------------------------");
          echo("");
-         echo(" SIMULATION PASSED            :-)");
+         echo(" SIMULATION PASSED :-)");
          echo("");
          echo("-----------------------------------------------------------------");
       else
          echo("-----------------------------------------------------------------");
          echo("");
-         echo("SIMULATION FAILED             :-(");
+         echo("SIMULATION FAILED :-(");
          echo("");
          echo("-----------------------------------------------------------------");
       end if;
 
-      SimFinished  <= true;
+      SimFinished <= true;
       wait;
    end process prStimuli;
 
